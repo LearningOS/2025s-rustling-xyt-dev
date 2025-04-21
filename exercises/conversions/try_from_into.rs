c7abcc3416e1a8edf9d99be80a413783aa6a630a
xyt-dev <417639959@qq.com>
2025-04-22 01:28:15 +0800

    updaaaaate

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -41,6 +39,14 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        match tuple {
+            (r, g, b) if (0..=255).contains(&r) 
+                        && (0..=255).contains(&g) 
+                        && (0..=255).contains(&b) => {
+                Ok(Color { red: r as u8, green: g as u8, blue: b as u8 })
+            }
+            _ => Err(Self::Error::IntConversion)
+        }
     }
 }
 
@@ -48,6 +54,14 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        match arr {
+            [r, g, b] if (0..=255).contains(&r) 
+                        && (0..=255).contains(&g) 
+                        && (0..=255).contains(&b) => {
+                Ok(Color { red: r as u8, green: g as u8, blue: b as u8 })
+            }
+            _ => Err(Self::Error::IntConversion)
+        }
     }
 }
 
@@ -55,6 +69,17 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 { return Err(Self::Error::BadLen) }
+        let arr = unsafe { *(&slice[0..=2] as *const [i16] as *const [i16; 3]) };
+        match arr {
+            [r, g, b] if (0..=255).contains(&r) 
+                        && (0..=255).contains(&g) 
+                        && (0..=255).contains(&g) 
+                        && (0..=255).contains(&b) => {
+                Ok(Color { red: r as u8, green: g as u8, blue: b as u8 })
+            }
+            _ => Err(Self::Error::IntConversion)
+        }
     }
 }
 
@@ -191,3 +216,5 @@ mod tests {
         assert_eq!(Color::try_from(&v[..]), Err(IntoColorError::BadLen));
     }
 }
+
+
